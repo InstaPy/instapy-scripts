8ed443c354385a3d9d73a5c81ba6b1b14005c0c4
anoff <offenhaeuser@gmail.com>
2019-02-08 21:16:43 +0100

    :star2: feat: parse all arguments and use default PATH resolution for python binary

--- run_instapy_only_once_for_mac.sh
+++ run_instapy_only_once_for_mac.sh
@@ -2,14 +2,23 @@
 
 # This is a script to mitigate possibility of multiple parallel cron jobs being triggered(discussed here: https://github.com/timgrossmann/InstaPy/issues/1235)
 # The following is an example of a cron scheduled every 10 mins
-# */10 * * * * bash /path/to/instapy-scripts/run_instapy_only_once_for_mac.sh target_followers_of_similar_accounts_and_influencers.py
+# */10 * * * * bash /path/to/instapy-scripts/run_instapy_only_once_for_mac.sh /path/to/instapy/quickstart_templates/target_followers_of_similar_accounts_and_influencers.py
 
-TEMPLATE_NAME=$1
-if ps aux | grep $TEMPLATE_NAME | awk '{ print $11 }' | grep python
+TEMPLATE_PATH=$1
+USERNAME=$2
+PASSWORD=$3
+if [ -z "$3" ]
 then
-   echo $TEMPLATE_NAME" is already running"
+   echo "Error: Missing arguments"
+   echo "Usage: bash $0 <script-path> <username> <password>"
+   exit 1
+fi
+
+if ps aux | grep $TEMPLATE_PATH | awk '{ print $11 }' | grep python
+then
+   echo "$TEMPLATE_PATH is already running"
 else
-   echo "Starting "$TEMPLATE_NAME
-   /Users/ishandutta2007/.pyenv/shims/python `dirname "$0"`/../Instapy/quickstart_templates/$TEMPLATE_NAME -u myusername -p mypwd --headless-browser --disable_image_load
+   echo "Starting $TEMPLATE_PATH"
+   python $TEMPLATE_PATH -u $USERNAME -p $PASSWORD --headless-browser --disable_image_load
 fi
 
